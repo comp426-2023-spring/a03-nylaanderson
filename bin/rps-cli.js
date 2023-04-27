f8f5ff5e031618fc1c0e60e3b4d2dce9dde28fe9
nylaanderson <69765415+nylaanderson@users.noreply.github.com>
2023-04-27 14:43:15 -0400

    Added condition for when args are out of range.

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -29,4 +29,23 @@ if (args.r || args.rules) {
   	- Rock CRUSHES Scissors
 	`);
 	process.exit(0);
-}	
+}
+
+if (args._.length > 1) {
+	console.error("Arguments are out of range.")
+	console.log(`Rules for Rock Paper Scissors:
+  	- Scissors CUTS Paper
+  	- Paper COVERS Rock
+  	- Rock CRUSHES Scissors
+	Usage: node-rps [SHOT]
+	Play Rock Paper Scissors (RPS)
+  		-h, --help      display this help message and exit
+  	-r, --rules     display the rules and exit
+	Examples:
+  	node-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  	node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+	process.exit(0);
+}
+	
