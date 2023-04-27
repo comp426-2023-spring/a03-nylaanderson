070939ff998894dfb1c24c6c1cc23768a4eca9ad
nylaanderson <69765415+nylaanderson@users.noreply.github.com>
2023-04-27 15:59:28 -0400

    added use case functions to rpsls-cli.js

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -36,4 +36,55 @@ if (args.r || args.rules) {
 	process.exit(0);
 }
 
+if(args._.length > 1) {
+	console.error("Arguements are out of range."
+	console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+  	- Scissors CUTS Paper
+  	- Paper COVERS Rock
+  	- Rock SMOOSHES Lizard
+  	- Lizard POISONS Spock
+  	- Spock SMASHES Scissors
+  	- Scissors DECAPITATES Lizard
+  	- Lizard EATS Paper
+  	- Paper DISPROVES Spock
+  	- Spock VAPORIZES Rock
+  	- Rock CRUSHES Scissors
+	Usage: node-rpsls [SHOT]
+	Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+  	-h, --help        display this help message and exit
+  	-r, --rules       display the rules and exit
+	Examples:
+  	node-rpsls        Return JSON with single player RPSLS result.
+                    e.g. {"player":"rock"}
+  	node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`
+	);
+	process.exit(0);
+}
+else if (args._.length ==== 0) {
+	console.log(JSON.stringify(rpsls.rpslsStd()));
+}
+else{
+	let player_choice = args._[0].toLowerCase();
+	if (rpsls.rpsls_choices.includes(player_choice)) {
+		console.log(JSON.stringify(rpsls.rpsls(player_choice)))
+	}
+	else {
+		console.error(` Invalid argument.
+         Acceptable arguments: rock | paper | scissors | lizard | spock
+         `);
+		console.log(
+	`- Scissors CUTS Paper
+  	- Paper COVERS Rock
+  	- Rock SMOOSHES Lizard
+  	- Lizard POISONS Spock
+  	- Spock SMASHES Scissors
+  	- Scissors DECAPITATES Lizard
+  	- Lizard EATS Paper
+  	- Paper DISPROVES Spock
+  	- Spock VAPORIZES Rock
+  	- Rock CRUSHES Scissors          
+	`);
+	}	
+}	
 
