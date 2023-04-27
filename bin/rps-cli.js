694007caf92432534dfab3ad30c506e6f2e1ee04
nylaanderson <69765415+nylaanderson@users.noreply.github.com>
2023-04-27 14:06:52 -0400

    Added rps help function

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -2,3 +2,21 @@
 
 import rpsls from "../lib/rpsls.js";
 import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+
+if (args.h || args.help) {
+	console.log(`Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+
+  -h, --help      display this help message and exit
+  -r, --rules     display the rules and exit
+
+Examples:
+  node-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}
+	`)
+	process.exit(0);
+}	
