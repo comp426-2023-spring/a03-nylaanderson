5f0ecd89e628c80608787b68713493e444b9395a
nylaanderson <69765415+nylaanderson@users.noreply.github.com>
2023-04-27 14:31:07 -0400

    Added rps rules function

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -7,16 +7,26 @@ const args = minimist(process.argv.slice(2));
 
 if (args.h || args.help) {
 	console.log(`Usage: node-rps [SHOT]
-Play Rock Paper Scissors (RPS)
+	Play Rock Paper Scissors (RPS)
 
-  -h, --help      display this help message and exit
-  -r, --rules     display the rules and exit
+  	-h, --help      display this help message and exit
+ 	-r, --rules     display the rules and exit
 
-Examples:
-  node-rps        Return JSON with single player RPS result.
+	Examples:
+  	node-rps        Return JSON with single player RPS result.
                   e.g. {"player":"rock"}
-  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+  	node-rps rock   Return JSON with results for RPS played against a simulated opponent.
                   e.g {"player":"rock","opponent":"scissors","result":"win"}
 	`)
+	process.exit(0);
+}
+
+if (args.r || args.rules) {
+	console.log(`Rules for Rock Paper Scissors:
+
+  	- Scissors CUTS Paper
+  	- Paper COVERS Rock
+  	- Rock CRUSHES Scissors
+	`);
 	process.exit(0);
 }	
